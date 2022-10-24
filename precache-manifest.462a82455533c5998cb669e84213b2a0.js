a6e4ef9834d9217b7be418ba4f4325d14ecac4ed
vanha777 <vanha777@icloud.com>
2022-10-25 05:54:52 +1100

    Updates

---
+++ precache-manifest.462a82455533c5998cb669e84213b2a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "310875ba918d63127769",
+    "url": "/RoboFriendsProject/static/css/main.835a12d9.chunk.css"
+  },
+  {
+    "revision": "310875ba918d63127769",
+    "url": "/RoboFriendsProject/static/js/main.310875ba.chunk.js"
+  },
+  {
+    "revision": "31088909e753e45fc079",
+    "url": "/RoboFriendsProject/static/js/1.31088909.chunk.js"
+  },
+  {
+    "revision": "54992cf85ffe9267eebe",
+    "url": "/RoboFriendsProject/static/js/2.54992cf8.chunk.js"
+  },
+  {
+    "revision": "37b25d1e3e4a79d19c52",
+    "url": "/RoboFriendsProject/static/js/runtime~main.37b25d1e.js"
+  },
+  {
+    "revision": "88c385081a83923e5b8037c96c6d70c0",
+    "url": "/RoboFriendsProject/static/media/font.88c38508.ttf"
+  },
+  {
+    "revision": "1649349200c8ce03db726d7d6de32836",
+    "url": "/RoboFriendsProject/index.html"
+  }
+];
